10a66a65df42b858bad5ab8dd871b121b7d594d1
MA <manuel@ckdatagroup.com>
2025-08-29 12:36:09 +0100

    Updated assignments

--- Basic JavaScript Projects/Project1_expressions_alert/js/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -1,2 +1,23 @@
 
-document.write("Hello World!");
+var A= "Hello World!"; //variable assignment
+document.write(A); //the write function is deprecated
+
+let message1 = 'It\'s a beautiful day!'; //escape character
+let message2 = "She said, \"Hello there!\""; //escape character
+
+window.alert(message1); //alert function
+window.alert(message2); 
+
+//concatenate function
+document.write("\"Be who you and say what you feel, "
++ "because those who mind don\'t matter, and those who matter don\'t mind.\""
++ "- Dr. Seuss");
+
+//multiple variables
+var Family = "The Arezzinis", Dad = "Jacob", Mom = "Beth", 
+Son = "Josh", Daughter = "Emily";
+document.write(Dad)
+
+//writing an expression
+let result = 5+10;
+window.alert ("The result of 5 + 10 is: " + result);

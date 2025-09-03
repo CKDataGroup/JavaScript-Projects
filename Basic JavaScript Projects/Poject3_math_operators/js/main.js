d55d5a9dbdecd3dd55f88406bed500ff1ee61f35
MA <manuel@ckdatagroup.com>
2025-09-03 11:47:26 +0100

    Updated with all the assignments

---
+++ Basic JavaScript Projects/Poject3_math_operators/js/main.js
@@ -0,0 +1,55 @@
+function addition_Function() {
+  const result = 2 + 2;
+  document.getElementById("Addition").innerHTML = "2 + 2 = " + result;
+}
+
+function subtraction_Function() {
+  const result = 10 - 3;
+  document.getElementById("Subtraction").innerHTML = "10 - 3 = " + result;
+}
+
+function multiplication_Function() {
+  const result = 6 * 7;
+  document.getElementById("Multiplication").innerHTML = "6 × 7 = " + result;
+}
+
+function division_Function() {
+  const result = 21 / 3;
+  document.getElementById("Division").innerHTML = "21 ÷ 3 = " + result;
+}
+function more_Math_Function() {
+  // Example of a combined operation
+  const result = (1 + 2) * (3 + 4) - 5 / 5;
+  document.getElementById("more_Math").innerHTML =
+    "(1 + 2) × (3 + 4) − 5 ÷ 5 = " + result;
+}
+function modulus_Operator() {
+  const result = 25 % 6;
+  document.getElementById("modulus").innerHTML = 
+  "25 % 6  = " + result;
+}
+function negation_Operator() {
+  const x = 10;
+  document.getElementById("negation").innerHTML = -x;
+}
+function increment_Operator() {
+  var x = 5;
+  x++;
+  document.getElementById("increment").innerHTML = x;  
+}
+function decrement_Operator() {
+  var x = 5.25;
+  x--;
+  document.getElementById("decrement").innerHTML = x;
+}  
+function random_Number() {
+  var x = Math.random() * 100;
+  document.getElementById("random").innerHTML = x;
+}
+function math_object() {  //I am choosing the square root function
+  const num = 25;
+  const result = Math.sqrt(num);
+  document.getElementById("math_object").innerHTML = "The square root of " + num + " is " + result;
+}
+
+    // It is working on all the operators
